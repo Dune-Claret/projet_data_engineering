501b49a3fe2b4ce0ca48d06c995ae2f67a7694fe
tsio236 <66494583+tsio236@users.noreply.github.com>
2021-01-24 20:30:42 +0100

    Add files via upload

--- dash/france.py
+++ dash/france.py
@@ -13,281 +13,11 @@ import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Imports de fichiers en local
-#from src.paths import francePath, countriesPath
-#from src.process_data import process_france
+# Import local
+from requests_mongo import get_graphes
 
-# Chargement des donnees
-#france = pd.read_csv(francePath, index_col=0)
-
-#with open(countriesPath) as f:
-#    countriesGeoJson = json.load(f)
-
-# Traitement des donnees
-#france = process_france(france)
-
-# Fonctions pour la page
-def graph_map_france(year):
-    """
-    Retourne une carte representant le pourentage d'obesite pour
-    chaque pays dans le monde pour une annee donnee.
-    """
-
-    # Filtre les donnees par rapport a l'annee
-    #df = france.query("year == {} and sex == 'B'".format(year))
-
-    # Creation de la map
-    #mapFrance = px.choropleth_mapbox(
-    #    data_frame = df,
-    #    geojson = countriesGeoJson,
-    #    locations = "country_code",
-    #    color = "france",
-    #    color_continuous_scale="Brwnyl",
-    #    range_color=(france.france.min(), france.france.max()),
-    #    mapbox_style = 'carto-darkmatter',
-    #    center = {"lat": 33.0902, "lon": -35.7129},
-    #    zoom = 1,
-    #    hover_name = df.country,
-    #    hover_data = ["france", "continent"]
-    #)
-
-    #mapFrance.update_layout(
-    #    margin=dict(l=0, r=0, t=0, b=0),
-    #    paper_bgcolor='rgba(0,0,0,0)',
-    #    legend_title_text='France (%)',
-    #    font=dict(
-    #        color="white",
-    #        size=12
-    #    )
-    #)
-    #mapFrance.layout.coloraxis.colorbar.title = "France (%)"
-    #mapFrance.layout.coloraxis.colorbar.x = 0.9
-
-    #return mapFrance
-    return 0
-
-def graph_bar_france(year):
-    """
-    Retourne un histogramme representant la distribution des
-    pourcentage d'obesite selon les pays pour une annee donnee.
-    """
-
-    # Filtre les donnees par rapport a l'annee
-    #df = france.query("year == {} and sex == 'B'".format(year))
-    
-    # Le pas
-    #step = 2.5
-    
-    # Creation des donnees
-    #hist, bins = np.histogram(
-    #    df.france,
-    #    bins = np.arange(
-    #        france.france.min(),
-    #        france.france.max()+step,
-    #        step
-    #    )
-    #)
-
-    # Creation de l'histogramme
-    #histogramFrance = go.Figure(
-    #    data = [
-    #        go.Bar(
-    #            x = [str(str(int(eBin)) + "-" + str(int(eBin+step)) + "%") for eBin in bins],
-    #            y = hist,
-    #            text = hist,
-    #            marker={'color': bins,'colorscale': 'Brwnyl'}
-    #        )
-    #    ]
-    #)
-
-    #histogramFrance.update_layout(
-    #    height=330,
-    #    bargap=0,
-    #    margin=dict(l=8, r=8, t=8, b=0),
-    #    paper_bgcolor='rgba(38,38,38,1)',
-    #    plot_bgcolor='rgba(38,38,38,1)',
-    #    yaxis_visible=False,
-    #    yaxis_showticklabels=False,
-    #)
-
-    #histogramFrance.layout.xaxis.color = 'white'
-    
-    #histogramFrance.update_traces(
-    #    texttemplate='%{text:.2s}',
-    #    textposition='outside',
-    #    textfont=dict(
-    #        size=12,
-    #        color="white"
-    #    )
-    #)
-
-    #return histogramFrance
-    return 0
-
-def graph_line_france_group(pMinYear, pMaxYear, pGroupType, pLocation):
-    """
-    Retourne un graphique avec deux courbes representant l'evolution
-    de l'obesite au sein d'un pays/continent pour chaque sexe dans un
-    interval donne.
-    """
-
-    # Filtre selon le groupe
-    #group_filter = '{} == "{}"'.format("country" if pGroupType == "Countries" else "continent", pLocation) 
-
-    # Filtre pour l'interval
-    #interval_filter = "year >= {} and year <= {}".format(pMinYear, pMaxYear)
-
-    # Filtre pour le sex
-    #sex_filter = "sex != 'B'"
-
-    # Filtre les donnees
-    #df = france.query(interval_filter + " and " + group_filter + " and " + sex_filter)
-
-    # Regroup si continent
-    #if pGroupType != "Countries":
-    #    df = df.groupby(["year", "sex"]).mean().reset_index(level=['year', 'sex']).dropna()
-    
-
-    # Creation du graphique
-    #try:
-    #    lineplot = px.line(
-    #        df.replace({"sex":{"M":"Male", "F":"Female"}}),
-    #        x="year",
-    #        y="france",
-    #        color="sex"
-    #    )
-
-        # Modification d'elements du graphique
-        #lineplot.update_layout(
-        #    title="Evolution of france percentage by sex",
-        #    legend_title="",
-        #    margin=dict(l=4, r=4, t=40, b=4),
-        #    paper_bgcolor='rgba(48,48,48,1)',
-        #    plot_bgcolor='rgba(48,48,48,1)',
-        #    font=dict(
-        #        size=12,
-        #        color="white"
-        #    ),
-        #    xaxis=dict(title="Year"),
-        #    yaxis=dict(title="france (%)")
-        #)
-
-
-        #return lineplot
-    #except:
-        #return None
-    return 0
-
-def graph_pie_france_group(year, pGroupType, pLocation):
-    """
-    Retourne un graphique camenbert representant:
-    - Un pays par rapport a son continent
-    - Un continent par rapport au monde
-    Pour une annee, type de region et la region donnee.
-    """
-
-    #pie_chart = None
-
-    # Filtre annee
-    #year_filter = "year == {}".format(year)
-
-    # Filtre group
-    #group_filter = '{} == "{}"'.format("country" if pGroupType == "Countries" else "continent", pLocation)
-
-    # Filtre sex
-    #sex_filter = "sex == 'B'"
-
-    # Filtre les donnees
-    #and_space = " and "
-    #df = france.query(year_filter + and_space + group_filter + and_space + sex_filter)
-
-    # Cas d'un pays
-    #if pGroupType == "Countries":
-        # Donnees pour le camenbert
-        #try:
-            #obese = float(df.france)
-        #except:
-            #obese = 0
-        
-    # Cas d'un continent
-    #else:
-        # Donnees pour le camenbert
-        #try:
-            #obese = float(df.france.mean())
-        #except:
-            #obese = 0
-    
-    #labels = ["Obese", "Not obese"]
-    #values = [obese, 100 - obese]
-
-    # Creation du camenbert
-    #pie_chart = go.Figure(data=[
-    #    go.Pie(
-    #        labels=labels,
-    #        values=values,
-    #        pull=[0.25, 0],
-    #    ),
-    #])
-
-    # Modification d'elements du graphique
-    #pie_chart.update_layout(
-    #    title="Percentage of france in population",
-    #    legend_title="",
-    #    margin=dict(l=4, r=4, t=40, b=4),
-    #    paper_bgcolor='rgba(48,48,48,1)',
-    #    plot_bgcolor='rgba(48,48,48,1)',
-    #    font=dict(
-    #        size=12,
-    #        color="white"
-    #    ),
-    #)
-
-    #pie_chart.update_traces(
-    #    textposition='outside',
-    #)
-
-    #return pie_chart
-    return 0
-
-def rank_france_group(year, pGroupType, pLocation):
-    """
-    Retourne un tuple (rank, n) tel que:
-    - rank : le rang du pays/continent par rapport a ses confreres
-    - n    : le nombre de confreres 
-    La position retournee est recuperee par un tri croissant des donnees
-    donc plus le rang obtenue s'approche du nombre de confreres plus le
-    pourcentage d'obesite est eleve.
-    """
-
-    #rank, n = None, None
-
-    # Pre-selection
-    #franceSelect = france.query('year == {} and sex == "B"'.format(year))
-
-    # Tri des donnees
-    #franceSelectSorted = franceSelect.sort_values(by="france").copy()
-
-    # Regroupement pour les continents
-    #if pGroupType == "Continents":
-    #    franceSelectSorted = franceSelectSorted.groupby("continent").mean()
-
-    # Reset l'index
-    #new_index = np.arange(1, franceSelectSorted.shape[0] + 1)
-    #if pGroupType == "Countries":
-    #    franceSelectSorted = franceSelectSorted.set_index(new_index)
-    #else:
-    #    franceSelectSorted = franceSelectSorted[['france']].reset_index(level='continent').set_index(new_index)
-
-    #try:
-        # Recupere le rang
-        #rank = franceSelectSorted.index[franceSelectSorted['country' if pGroupType == "Countries" else 'continent'] == pLocation].tolist()
-        #n = new_index.max()
-
-        #return rank[0], n
-
-    #except:
-        #return None, None
-    return 0
+# Variables pour la page
+f1, f2, f3 = get_graphes('France')
 
 def generate_dropdown(dataframe, feature):
     dropdown = list()
@@ -296,28 +26,6 @@ def generate_dropdown(dataframe, feature):
         dropdown.append({'label': element, 'value':element})
 
     return dropdown
-
-
-# Variables pour les elements de la page
-minYear = 1952 #france.year.min()
-maxYear = 2020 #france.year.max()
-dropdown_continents = ["Amérique du Nord", "Amérique du Sud", "Europe", 'Asie', 'Océanie'] #generate_dropdown(france, 'continent')
-dropdown_countries = ["USA", "France", "Australie"] #generate_dropdown(france, 'country')
-
-# Selection du type de groupe
-radioitems = dbc.FormGroup(
-    [
-        html.Li([dbc.Label("Select a group type")]),
-        dbc.RadioItems(
-            options=[
-                {"label": "Country", "value": "Countries"},
-                {"label": "Continent", "value": "Continents"},
-            ],
-            value="Countries",
-            id="radioitems-france-group",
-        ),
-    ]
-)
 
 # Page pour France
 pageFrance = html.Div([
@@ -419,18 +127,18 @@ pageFrance = html.Div([
 
                                 dbc.CardFooter([
                                     # Label des annees du slider
-                                    dbc.Label(
-                                        "From {} to {}:".format(minYear, maxYear),
-                                        id="label-france-group-graph",
-                                    ),
+                                    #dbc.Label(
+                                    #    "From {} to {}:".format(minYear, maxYear),
+                                    #    id="label-france-group-graph",
+                                    #),
 
                                     # Selection de l'interval
-                                    dcc.RangeSlider(
-                                        id="slider-france-group-graph",
-                                        min=minYear,
-                                        max=maxYear,
-                                        value=[minYear, maxYear]
-                                    )
+                                    #dcc.RangeSlider(
+                                    #    id="slider-france-group-graph",
+                                    #    min=minYear,
+                                    #    max=maxYear,
+                                    #    value=[minYear, maxYear]
+                                    #)
                                 ])
                             ], outline=True, color="dark")
                         ], style={"flex":3, "padding-right":5}),
@@ -454,18 +162,18 @@ pageFrance = html.Div([
                                 # Pied de page des graphiques
                                 dbc.CardFooter([
                                     # Label de l'annee selectionnee
-                                    dbc.Label(
-                                        "During the year {}".format(minYear),
-                                        id="label-france-miscellaneous"
-                                    ),
+                                    #dbc.Label(
+                                    #    "During the year {}".format(minYear),
+                                    #    id="label-france-miscellaneous"
+                                    #),
 
                                     # Selection de l'annee
-                                    dcc.Slider(
-                                        id="slider-france-miscellaneous-graph",
-                                        min=minYear,
-                                        max=maxYear,
-                                        value=minYear
-                                    )
+                                    #dcc.Slider(
+                                    #    id="slider-france-miscellaneous-graph",
+                                    #    min=minYear,
+                                    #    max=maxYear,
+                                    #    value=minYear
+                                    #)
                                 ]),
                             ], color="dark", outline=True),
 
@@ -488,11 +196,14 @@ pageFrance = html.Div([
     # Cote droit
     html.Div(
         [
-            # Graphique: Carte choropleth France
-            html.Div([dcc.Graph(id='graph-map-france')], style = {}),
+            # Graphique 1
+            html.Div([dcc.Graph(id='f1')], style = {}),
 
-            # Graphique: Distribution France
-            html.Div([dcc.Graph(id='graph-bar-france')], style = {}),
+            # Graphique 2
+            html.Div([dcc.Graph(id='f2')], style = {}),
+
+            # Graphique 3
+            html.Div([dcc.Graph(id='f3')], style = {}),
         ],
         style = {"flex":2, "display": "flex", "flex-direction": "column"},
     ),],
--- dash/usa.py
+++ dash/usa.py
@@ -12,233 +12,15 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Imports de fichiers en local
-#from src.paths import usaPath
-#from src.process_data import process_usa
-#from src.obesity_page import generate_dropdown
-
-
-
-# Chargement des donnees
-#usa = pd.read_csv(usaPath)
-
-# Traitement des donnees
-#usa = process_usa(usa)
-
-# Fonctions pour la page
-def check_values_available(pCountry):
-    """
-    Verifie s'il existe des valeurs ppur les sexes M et F pour un pays.
-    """
-    # Creer un vecteur qui compte le nombre de valeurs different de zero
-    #vector = usa.query('country == "{}"'.format(pCountry)).sex.value_counts().values
-
-    # Retourne vrai s'il n'y a pas que des zeros
-    #return np.sum(np.nonzero(vector)) != 0
-    return 0
-
-def generate_input(title, input_widget):
-    """
-    Generate an input block with
-    - a title
-    - an input widget (Select from dash_bootstrap for example)
-    """
-
-    return html.Div([
-        # Titre de l'input
-        html.Li([
-            html.H6(title)
-        ]),
-
-        # Input
-        input_widget
-    ])
-
-def generate_sep():
-    """
-    Generate a separator composed of:
-    - line break
-    """
-    return html.Div([
-        html.Br(),
-    ])
-
-def graph_line_subjects_country_range(pMinYear, pMaxYear, pCountry, pActivity, pSex):
-    """
-    Retourne un graphique lineplot des activites d'un pays dans un interval donne pour un sexe.
-    """
-
-    #data = usa.query('year >= {} and year <= {} and country == "{}" and activity == "{}" and sex == "{}"'.format(pMinYear, pMaxYear, pCountry, pActivity, pSex))
-
-    #lineplot = px.area(
-    #    data, 
-    #    x="year", 
-    #    y="value", 
-    #    color="subject", 
-    #    line_group="subject",
-    #    hover_name="subject",
-    #    color_discrete_sequence=px.colors.sequential.haline,
-    #    height=560
-    #) if data.shape[0] > 0 else go.Figure()
-
-    #lineplot.update_layout(
-    #    title="USA by sector between {} and {} in {}".format(pMinYear, pMaxYear, pCountry),
-    #    showlegend=False,
-    #    margin=dict(l=4, r=4, t=40, b=4),
-    #    paper_bgcolor='rgba(48,48,48,1)',
-    #    plot_bgcolor='rgba(48,48,48,1)',
-    #    font=dict(
-    #        size=12,
-    #        color="white"
-    #    ),
-    #    xaxis=dict(title="Year"),
-    #    yaxis=dict(title="Number of employees")
-    #)
-
-    #return lineplot
-    return 0
-
-def graph_pie_subjects_country_year(pYear, pCountry, pActivity, pSex):
-    """
-    Retourne un graphique piechart des activites d'un pays pour une annee et un sexe donnes.
-    """
-
-    #data = usa.query('year == {} and country == "{}" and activity == "{}" and sex == "{}"'.format(pYear, pCountry, pActivity, pSex))
-
-    #pie = px.pie(
-    #    data,
-    #    values='value',
-    #    hover_name="subject",
-    #    names='subject',
-    #    color_discrete_sequence=px.colors.sequential.haline
-    #) if data.shape[0] > 0 else go.Figure()
-
-    #pie.update_layout(
-        #showlegend=False,
-        #margin=dict(l=4, r=4, t=40, b=4),
-        #paper_bgcolor='rgba(48,48,48,1)',
-        #plot_bgcolor='rgba(48,48,48,1)',
-        #title="{} jobs in {} during {}".format("Desktop" if pActivity == "D" else "Manual", pCountry, pYear),
-        #legend = dict(
-            #bgcolor = 'rgba(0, 0, 0, 0)',
-            #yanchor="top",
-            #y=0.0,
-            #xanchor="left",
-            #x=0.0,
-        #),
-        #font=dict(
-        #    size=12,
-        #    color="white"
-        #)
-    #)
-
-    #return pie
-    return 0
-
-def graph_line_desktop_manual_country_range(pMinYear, pMaxYear, pCountry):
-    """
-    Retourne un graphique lineplot des activites bureautiques et manuelles d'un pays dans un interval donne.
-    """
-
-    # Recupere le total d'employes dans chaque activite
-    #data = usa.query('year >= {} and year <= {} and country == "{}" and activity != "U" and sex == "{}"'.format(pMinYear, pMaxYear, pCountry, "B")).groupby(["country", "year", "activity"]).sum().reset_index(level=[0, 1]).copy()
-    
-    # Traitement sur l'index
-    #data['activity'] = data.index
-    #data.index = np.arange(0, data.shape[0])
-    #data = data.replace({"activity":{"D":"Desktop", "M":"Manual"}})
-    #lineplot = None
-
-    #try:
-        #lineplot = px.line(
-            #data, 
-            #x="year", 
-            #y="value", 
-            #color="activity", 
-            #hover_name="value", 
-            #color_discrete_sequence=px.colors.sequential.haline,
-            #height=620)
-
-        #lineplot.update_traces(
-        #    line=dict(width=4),
-        #)
-
-    #except:
-    #    lineplot = go.Figure()
-
-    #lineplot.update_layout(
-    #    title="Type of activity between {} and {} in {}".format(pMinYear, pMaxYear, pCountry),
-    #    legend_title="Type of activity",
-    #    margin=dict(l=4, r=4, t=40, b=4),
-    #    paper_bgcolor='rgba(48,48,48,1)',
-    #    plot_bgcolor='rgba(48,48,48,1)',
-    #    font=dict(
-    #        size=12,
-    #        color="white"
-    #    ),
-    #    xaxis=dict(title="Year"),
-    #    yaxis=dict(title="Number of employees")
-    #)
-
-    #return lineplot
-    return 0
+# Import local
+from requests_mongo import get_graphes
 
 # Variables pour la page
-minYear = 1952 #usa.year.min()
-maxYear = 2020 #usa.year.max()
-
-# Input pour le pays
-input_country = dbc.Select(
-    id="input-country-usa",
-    options= ["USA", "Australie", "France"], #generate_dropdown(usa, 'country'),
-    value="France"
-)
-
-# Input pour l'interval des annees
-input_range_year = dcc.RangeSlider(
-    id="input-range-year-usa",
-    min=minYear,
-    max=maxYear,
-    step=1,
-    value=[minYear, maxYear],
-)
-
-# Input pour le sex
-input_sex = dbc.RadioItems(
-    id="input-sex-usa",
-    options=[
-        {"label":"Both", "value":"B"},
-        {"label":"Male", "value":"M"},
-        {"label":"Female", "value":"F"},
-        ],
-    value="B",
-    inline=True
-)
-
-# Input pour le type d'activite
-#input_activity = dbc.RadioItems(
-#    id="input-activity-usa",
-#    options=[
-#        {"label":"Desktop", "value":"D"},
-#        {"label":"Manual", "value":"M"},
-#        ],
-#    value="D",
-#    inline=True
-#)
-
-# Input pour l'annee
-input_year = dcc.Slider(
-    id="input-year-usa",
-    min=minYear,
-    max=maxYear,
-    step=1,
-    value=minYear
-)
+f1, f2, f3 = get_graphes('USA')
 
 # Menu
 menu = dbc.Col(
     html.Div([
-        # Carte définissant des limites visibles
         dbc.Card(
             [
                 # Titre
@@ -326,56 +108,16 @@ menu = dbc.Col(
     width=4
 )
 
-# Lineplot subjects
-lineplot_subjects = dbc.Col(
-    html.Div([
-        dbc.Card(
-            children=[dcc.Graph(id="lineplot-subjects-usa")],
-            body=True,
-        )
-    ]),
-    width=8
-)
-
-# Piechart
-piecharts_subjects = dbc.Col(
-    html.Div([
-        dbc.Card(
-            [
-                dbc.CardBody(dcc.Graph(id="piecharts-subjects-usa")),
-                dbc.CardFooter([dbc.Label(id="label-year-piechart-usa"), input_year])
-            ],
-            className="mb-3",
-            style={'height':660}
-        )
-    ]),
-    width=5
-)
-
-# Desktop/Manual lineplot
-#desktop_manual_graph = dbc.Col(
-#    html.Div([
-#        dbc.Card(
-#            dcc.Graph(id="lineplot-activity-usa"),
-#            body=True,
-#        )
-#    ]),
-#    width=7
-#)
-
-# Page pour l'emploi
 pageUsa = html.Div([
-    # Premiere ligne (menu, barchart subjects)
-    dbc.Row([
-        menu,
-        lineplot_subjects
-    ], style={"padding":"20px"}),
+    # Graphique 1
+    html.Div([dcc.Graph(id='f1')], style = {}),
 
-    # Deuxieme ligne (piecharts subjects, lineplot D/M)
-    dbc.Row([
-        piecharts_subjects,
-        #desktop_manual_graph
-    ], style={"padding":"20px"})
+    # Graphique 2
+    html.Div([dcc.Graph(id='f2')], style = {}),
 
-])
+    # Graphique 3
+    html.Div([dcc.Graph(id='f3')], style = {}),
+],
+style = {"flex":2, "display": "flex", "flex-direction": "column"}
+)
 
